ccd29abc6c6efc35ab9da1425e80d19240dbc7cc
Alexander Zhirov <aleks@aradmin.org.ru>
2012-05-08 21:40:17 +0700

    FEATURE Added support for value-parametrized benchmark

--- src/hayai.hpp
+++ src/hayai.hpp
@@ -53,3 +53,39 @@
                ::Hayai::Test,                            \
                runs,                                     \
                iterations)
+
+// Parametrized benchmarks support
+#define BENCHMARK_P_(fixture_name, benchmark_name, fixture_class_name, arguments)                       \
+class BENCHMARK_CLASS_NAME_(fixture_name, benchmark_name): public fixture_class_name {                  \
+public:                                                                                                 \
+  BENCHMARK_CLASS_NAME_(fixture_name, benchmark_name) () {}                                             \
+  virtual ~ BENCHMARK_CLASS_NAME_(fixture_name, benchmark_name) () {}                                   \
+protected:                                                                                              \
+  inline void TestPayload arguments;                                                                    \
+};                                                                                                      \
+void BENCHMARK_CLASS_NAME_(fixture_name, benchmark_name)::TestPayload arguments
+
+#define BENCHMARK_P(fixture_name, benchmark_name, arguments)                                            \
+  BENCHMARK_P_(fixture_name, benchmark_name, Hayai::Fixture, arguments)
+
+#define BENCHMARK_P_F(fixture_name, benchmark_name, arguments)                                            \
+  BENCHMARK_P_(fixture_name, benchmark_name, fixture_name, arguments)
+
+#define BENCHMARK_P_CLASS_NAME_(fixture_name, benchmark_name, id)                                       \
+  fixture_name ## _ ## benchmark_name ## _Benchmark_ ## id
+
+#define BENCHMARK_P_INSTANCE1(fixture_name, benchmark_name, runs, iterations, arguments, id)            \
+class BENCHMARK_P_CLASS_NAME_(fixture_name, benchmark_name, id):                                        \
+  public BENCHMARK_CLASS_NAME_(fixture_name, benchmark_name) {                                          \
+protected:                                                                                              \
+  virtual void TestBody() { this->TestPayload (arguments); }                                            \
+private:                                                                                                \
+  static const ::Hayai::TestDescriptor* _descriptor;                                                    \
+};                                                                                                      \
+const ::Hayai::TestDescriptor* BENCHMARK_P_CLASS_NAME_(fixture_name, benchmark_name, id)::_descriptor = \
+    ::Hayai::Benchmarker::Instance().RegisterTest(                                                      \
+        #fixture_name, #benchmark_name, runs, iterations,                                               \
+        new ::Hayai::TestFactoryDefault< BENCHMARK_P_CLASS_NAME_(fixture_name, benchmark_name, id) >());
+
+#define BENCHMARK_P_INSTANCE(fixture_name, benchmark_name, runs, iterations, arguments)                 \
+  BENCHMARK_P_INSTANCE1(fixture_name, benchmark_name, runs, iterations, arguments, __COUNTER__)
