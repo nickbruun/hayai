7052f4d2df05528a9f7cc26e72ff6b5963a673ab
Vlad Lazarenko <vlad@lazarenko.me>
2013-12-17 23:33:53 -0500

    Added some notes/docs on clock implementation.

--- src/hayai_chrono.hpp
+++ src/hayai_chrono.hpp
@@ -1,10 +1,36 @@
+//
+// System-specific implementation of the clock functions.
+//
+// On Apple (OS X, iOS), mach_absolute_time() is used. It gets
+// CPU/bus dependent real-time clock with up to nanosecond precision.
+//
+// On Linux, clock_gettime() is used to access monotonic real-time clock
+// with up to nanosecond precision. On kernels 2.6.28 and newer, the ticks
+// are also raw and are not subject to NTP and/or adjtime(3) adjustments.
+//
+// Other POSIX compliant platforms resort to using gettimeofday(). It is
+// subject to clock adjustments, does not allow for higher than microsecond
+// resolution and is also declared obsolete by POSIX.1-2008.
+//
+// Note on C++11:
+//
+// Starting with C++11, we could use std::chrono. However, the details of
+// what clock is really being used is implementation-specific. For example,
+// Visual Studio 2012 defines "high_resolution_clock" as system clock with
+// ~1 millisecond precision that is not acceptable for performance measurements.
+// Therefore, we are much better off having full control of what mechanism we
+// use to obtain the system clock.
+//
+
 #ifndef __HAYAI_CHRONO
 #define __HAYAI_CHRONO
 
 #if defined(__APPLE__)
 # include <mach/mach_time.h>
+#elif defined(__linux__)
+# include <time.h>
 #else
-#include <sys/time.h>
+# include <sys/time.h>
 #endif
 #include <stdint.h>
 
