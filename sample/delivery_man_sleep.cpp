4f4fc4814252575f10786c42f36721e15573d334
Nick Bruun <mail@nickbruun.dk>
2014-04-28 21:10:56 +0200

    Return value from msleep is not used, make it void.

--- sample/delivery_man_sleep.cpp
+++ sample/delivery_man_sleep.cpp
@@ -1,43 +1,44 @@
 #include <hayai.hpp>
 
-
 #ifdef _WIN32
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
 #include <windows.h>
 
-int msleep(unsigned val)
+inline void msleep(unsigned int duration)
 {
-    Sleep(val);
-	return 0;
+    Sleep(duration);
 }
 
 #else
-#include <stdio.h> 
 #include <unistd.h>
 
-int msleep(unsigned val)
+inline void msleep(unsigned int duration)
 {
-    return usleep(val*1000);
+    usleep(duration * 1000);
 }
 #endif
 
-// The delivery man need some sleep sometimes
-
-// Sleep for 1 ms 10 times
+// The delivery man need some sleep sometimes.
+//
+// Sleep for 1 ms 10 times.
 BENCHMARK(SomeSleep, Sleep1ms, 5, 10)
 {
     msleep(1);
 }
 
-// Sleep for 5 ms 10 times
+// The delivery man need some sleep sometimes.
+//
+// Sleep for 5 ms 10 times.
 BENCHMARK(SomeSleep, Sleep10ms, 5, 10)
 {
     msleep(10);
 }
 
-// Sleep for 20 ms 10 times
+// The delivery man need some sleep sometimes.
+//
+// Sleep for 20 ms 10 times.
 BENCHMARK(SomeSleep, Sleep20ms, 5, 10)
 {
     msleep(20);
